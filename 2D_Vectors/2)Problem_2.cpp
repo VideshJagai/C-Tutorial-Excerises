fa579abd4e8c147a6070d6549f22d79f0867a893
Videsh Jagai <94847951+VideshJagai@users.noreply.github.com>
2024-07-08 16:32:38 -0400

    Update 2)Problem_2.cpp

--- 2D_Vectors/2)Problem_2.cpp
+++ 2D_Vectors/2)Problem_2.cpp
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 /*
      In C++, Vector are called dynamic arrays that can automatically resize themseleves
