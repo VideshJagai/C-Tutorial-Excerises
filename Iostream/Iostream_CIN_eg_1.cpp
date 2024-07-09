49fbd7e798967997b2e9efacb8510381d752229f
Videsh Jagai <94847951+VideshJagai@users.noreply.github.com>
2024-07-09 09:32:43 -0400

    Update Iostream_CIN_eg_1.cpp

--- Iostream/Iostream_CIN_eg_1.cpp
+++ Iostream/Iostream_CIN_eg_1.cpp
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 //  C++ program to find the biggest value with the help of ternary operator
 
 
