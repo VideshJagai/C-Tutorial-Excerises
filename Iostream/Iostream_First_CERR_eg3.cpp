49e72b9a7573d687a172783d860a9f62e99564aa
Videsh Jagai <94847951+VideshJagai@users.noreply.github.com>
2024-07-09 09:34:18 -0400

    Update Iostream_First_CERR_eg3.cpp

--- Iostream/Iostream_First_CERR_eg3.cpp
+++ Iostream/Iostream_First_CERR_eg3.cpp
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 // We are using another function from the iostream header 
 // file to handle input and output stream in c++.
