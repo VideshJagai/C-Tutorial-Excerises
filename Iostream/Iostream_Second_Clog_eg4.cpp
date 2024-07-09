4ebfcdac7b0ccac5ff337e7737378dd619768223
Videsh Jagai <94847951+VideshJagai@users.noreply.github.com>
2024-07-09 09:34:37 -0400

    Update Iostream_Second_Clog_eg4.cpp

--- Iostream/Iostream_Second_Clog_eg4.cpp
+++ Iostream/Iostream_Second_Clog_eg4.cpp
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 // We are using the error function to trace any  error if it occurred; this is 
 // also available in the iostream file.
